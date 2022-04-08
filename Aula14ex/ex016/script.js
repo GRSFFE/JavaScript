4935f2c136dd01b874efa80497f6abdcb5f2dbeb
Germanno Resende <germannoresende@gmail.com>
2022-04-08 12:04:59 -0300

    Atualizei o ex016.

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -5,12 +5,17 @@ function contar() {
     var res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
         window.alert('[ERRO] Faltam Dados!')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         var i = Number(ini.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
         if (i < f) {
             //Contagem Crescente
             for (var c = i; c <= f; c += p) {
