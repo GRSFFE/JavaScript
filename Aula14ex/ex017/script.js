dbe683e5107e5857f92561c7f64ea82da92791cd
Germanno Resende <germannoresende@gmail.com>
2022-04-12 13:16:49 -0300

    Atualizei o ex017

--- Aula14ex/ex017/script.js
+++ Aula14ex/ex017/script.js
@@ -6,8 +6,15 @@ function tabuada() {
     } else {
        var n = Number(num.value) 
        var c = 1
-       while = (c <= 10) {
+        tab.innerHTML = '' 
+       while (c <= 10) {
+
            var item = document.createElement('option')
+           item.text = `${n} x ${c} = ${n*c}`
+           item.value = `tab${c}`
+           tab.appendChild(item)
+           c++
+
        }
     }
     
