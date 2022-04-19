f09ac5f63d8f03ce40b3ef652348ff4a24f586bd
Germanno Resende <germannoresende@gmail.com>
2022-04-19 15:27:19 -0300

    Variaveis compostas.

---
+++ Aula15/Ambiente.js
@@ -0,0 +1,12 @@
+var num = [5, 3, 2, 6, 8]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+var pos = num.indexOf(4)
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
